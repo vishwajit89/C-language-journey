ac0d55ea1e55b8cb1c587e9d9663841ea4ecc440
Solu kumar <harmonorelato12@gmail.com>
2026-07-04 18:09:56 +0530

    Create operator_evaluation.c
    add code to show step by step evaluation of 3*x/y - z+k, x=2 y=3 z=3 k=1, since 3*x/y happens in int first before storing in float e, this shows operator precedence and how int division loses decimal part before final result

---
+++ 03-operators/operator_evaluation.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	//explain step by step evaluation of 3*x/y - z+k
+	int x = 2, y = 3, z = 3, k = 1 ;
+	float e = 3*x/y - z + k ;
+	printf("the value of e is %f", e);
+	return 0 ;
+}
