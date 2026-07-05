9edb07053694ce293fc2cd465f6cbf3a4c376c3d
Solu kumar <harmonorelato12@gmail.com>
2026-07-05 23:10:11 +0530

    Create 04-conditions/if_statement.c
    add code to show if-else statement, when age is greater than 10 print inside if, else print age is not greater than 10, age = 5 so else block executes

---
+++ 04-conditions/if_statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+	int age = 30 ;
+	if (age >15){
+		printf ("your age is greater then 15");
+		printf ("we are inside if\n");
+	}
+if (age%4==2){ //renibder print hota ha 
+		printf ("your age is greater then 15");
+		printf ("we are inside if\n");
+}
+	return 0;
+	}
