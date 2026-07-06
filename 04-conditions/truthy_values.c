d7a222fc3ec1abeef1feabbf35b6bb83c8729d88
Solu kumar <harmonorelato12@gmail.com>
2026-07-06 10:50:30 +0530

    Create truthy_values.c
    add code to show which values are treated as true in c, if(1), if(6735), if('b') all execute since they are non zero, if(0) does not execute since 0 means false

---
+++ 04-conditions/truthy_values.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+	if (1){
+		printf("this if is executed\n");
+	}
+	if (6735){
+		printf("this if is executed\n");
+	}
+	if ('b') {
+		printf("this if is executed\n");
+	}
+	if (0){
+		printf("this if is executed\n");
+	}
+	return 0 ;
+}
