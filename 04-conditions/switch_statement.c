73a886ed9bd211dc5055e72eaa1a00e367505e21
Solu kumar <harmonorelato12@gmail.com>
2026-07-07 15:58:15 +0530

    Create switch_statement.c
    add code to show switch statement, takes input and prints matching case, if break is removed then all value after that case will print too

---
+++ 04-conditions/switch_statement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){
+	int a;
+	printf("enter a: ");
+	scanf("%d", &a);
+	switch(a){
+		case 1:
+		printf("you entered 1\n");
+		break;
+		case 2:
+		printf("you entered 2\n");
+		break;
+		case 3:
+		printf("you entered 3\n");
+		break;
+		case 4:
+		printf("you entered 4\n");
+	}
+	return 0;
+}
