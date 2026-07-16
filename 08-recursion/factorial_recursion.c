8aa1f4a930172c989456a2e54f872e73e10a3609
Solu kumar <harmonorelato12@gmail.com>
2026-07-16 20:13:14 +0530

    Create 08-recursion/factorial_recursion.c
    add code to find factorial using recursion, base condition when n is 0 or 1, function calls itself with n-1

---
+++ 08-recursion/factorial_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int factorial(int);
+
+int factorial(int n){
+	if(n == 1 || n == 0){
+		return 1;
+	}
+	return n*factorial(n-1);
+}
+int main(){
+	int a =4;
+	printf("the factorial of %d is %d",a, factorial(a));
+	return 0;
+}
