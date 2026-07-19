bdc93a71a90c0a9fc35feb24ce20633761711b28
Solu kumar <harmonorelato12@gmail.com>
2026-07-19 09:29:33 +0530

    Create double_pointer_basics.c
    add code to show double pointer, k stores address of pointer j which stores address of i, dereferencing twice gives original value

---
+++ 09-pointers/double_pointer_basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int i = 8;
+	int *j = &i;
+	int **k = &j;
+
+	printf("the value of i is %d\n", i);
+	printf("the value of i is %d\n", *j);
+	printf("the value of i is %d\n", *(&i));
+	printf("the value of i is %d\n", **(&j));
+	return 0;
+}
