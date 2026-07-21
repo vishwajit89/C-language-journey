4890f122f05df34486388d3d761c4651db48378a
Solu kumar <harmonorelato12@gmail.com>
2026-07-21 18:50:50 +0530

    Create modify_value_via_pointer.c
    add code to show pointer modifying original value, function multiplies x by 10 through its address

---
+++ 09-pointers/modify_value_via_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void change_to_ten_times(int*);
+void change_to_ten_times(int*a){
+	*a = *a * 10;
+}
+int main(){
+	int x = 45;
+	printf("the value of x is %d\n",x);
+	change_to_ten_times(&x);
+	printf("the value of x is %d\n",x);
+	return 0;
+}
