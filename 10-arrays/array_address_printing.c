4c5eb72fc015f446e64f840fbc00c52b9f7fb35b
Solu kumar <harmonorelato12@gmail.com>
2026-07-24 18:04:22 +0530

    Create array_address_printing.c
    add code to print address of each element in array, shows how consecutive elements are stored in memory

---
+++ 10-arrays/array_address_printing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int marks[5];
+
+    printf("Enter marks of 5 students:\n");
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &marks[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("The address of marks at index %d is %p\n", i, (void *)&marks[i]);
+    }
+
+    return 0;
+}
