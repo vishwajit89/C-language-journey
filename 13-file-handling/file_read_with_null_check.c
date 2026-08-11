d4238e53e5d62e4b23f439e3f1a03385854cd293
Solu kumar <harmonorelato12@gmail.com>
2026-08-11 23:15:03 +0530

    Create file_read_with_null_check.c
    add code to check if file exists before reading, fopen returns NULL if file doesn't exist so we check for that before using it

---
+++ 13-file-handling/file_read_with_null_check.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{ 
+    FILE *ptr;
+    ptr = fopen("harry.txt", "r");
+
+    if(ptr == NULL){
+        printf("The file does not exist sorry! \n");
+    }
+    else{
+        int num;
+        fscanf(ptr, "%d", &num);
+        printf("The value of num is %d \n", num);
+
+        fscanf(ptr, "%d", &num);
+        printf("The value of num is %d \n", num);
+
+        fclose(ptr);
+    }
+    return 0;
+}
