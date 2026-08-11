1af77f7d71767ac22bbf2f22ef3b1fb62c4e49d1
Solu kumar <harmonorelato12@gmail.com>
2026-08-11 23:13:50 +0530

    Create 13-file-handling/file_read_basic.c
    add code to read numbers from a file using fopen and fscanf, closes file at the end using fclose

---
+++ 13-file-handling/file_read_basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    FILE *ptr;
+    ptr = fopen("harry.txt", "r");
+    int num;
+    fscanf(ptr, "%d", &num);
+    printf("The value of num is %d \n", num);
+
+    fscanf(ptr, "%d", &num);
+    printf("The value of num is %d \n", num);
+
+    fclose(ptr);
+    
+    return 0;
+}
